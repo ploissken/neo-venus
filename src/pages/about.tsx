5160e7342e6f4496ced82c2543e375d5d4d14cce
rodurico <ploissken@gmail.com>
2025-03-27 17:19:56 -0300

    adding skeleton sign in; some cleanup

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,10 +2,16 @@ import Link from "next/link";
 
 export default function About() {
   return (
-    <>
-      <div>About</div>
-      <Link href="/about">About Us</Link>
-      <Link href="/user-profile">User Profile</Link>
-    </>
+    <ul>
+      <li>
+        <Link href="/user-profile">User Profile</Link>
+      </li>
+      <li>
+        <Link href="/about">About Us</Link>
+      </li>
+      <li>
+        <Link href="/sign-in">Sign In</Link>
+      </li>
+    </ul>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
 import Link from "next/link";
-import Button from "@mui/material/Button";
 
 export default function Index() {
   return (
-    <>
-      <div>Home</div>
-      <Button variant="contained">Hello world</Button>
-      <ul>
-        <li>
-          <Link href="/user-profile">User Profile</Link>
-        </li>
-        <li>
-          <Link href="/about">About Us</Link>
-        </li>
-      </ul>
-    </>
+    <ul>
+      <li>
+        <Link href="/user-profile">User Profile</Link>
+      </li>
+      <li>
+        <Link href="/about">About Us</Link>
+      </li>
+      <li>
+        <Link href="/sign-in">Sign In</Link>
+      </li>
+    </ul>
   );
 }
