90a0ad5728cebe75463750624cc44f124cb77db7
rodurico <ploissken@gmail.com>
2025-04-06 19:49:29 -0300

    add base chart with signs

--- src/app/user-profile/components/MapView.tsx
+++ src/app/user-profile/components/MapView.tsx
@@ -1,23 +1,24 @@
-import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import { ChartPlanet, Planet, ZodiacSign } from "@/lib/chart.types";
-import ZodiacSignIcon from "@/app/components/ZodiacSignIcon";
+import { ChartPlanet, Planet } from "@/lib/chart.types";
 import PlanetIcon from "@/app/components/PlanetIcon";
+import BaseChart from "./BaseChart";
 
 interface MapViewProps {
   chartPlanets: Array<ChartPlanet>;
 }
 
 export default function MapView({ chartPlanets = [] }: MapViewProps) {
+  const size = 500;
   return (
     <Grid
       size={12}
       sx={{
+        m: 4,
         position: "relative",
         top: 0,
         left: 0,
-        width: "400px",
-        height: "400px",
+        width: size,
+        height: size,
       }}
     >
       <div
@@ -27,40 +28,10 @@ export default function MapView({ chartPlanets = [] }: MapViewProps) {
           left: 0,
         }}
       >
-        <svg width="400" height="400" viewBox="0 0 400 400">
-          <circle
-            cx="200"
-            cy="200"
-            r="180"
-            stroke="gray"
-            strokeWidth="3"
-            fill="transparent"
-          />
-          {[0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330].map(
-            (angle) => (
-              <line
-                x1="200"
-                y1="200"
-                x2={200 + 180 * Math.cos((angle * Math.PI) / 180)}
-                y2={200 + 180 * Math.sin((angle * Math.PI) / 180)}
-                stroke="gray"
-                stroke-width="1"
-              />
-            )
-          )}
-
-          <circle
-            cx="200"
-            cy="200"
-            r="140"
-            stroke="gray"
-            strokeWidth="3"
-            fill="#111"
-          />
-          <circle cx="200" cy="200" r="60" fill="#333" />
-        </svg>
+        <BaseChart size={size} rotationDegrees={-35} />
       </div>
 
+      {/* planets */}
       <div
         style={{
           position: "absolute",
@@ -68,7 +39,7 @@ export default function MapView({ chartPlanets = [] }: MapViewProps) {
           left: 0,
         }}
       >
-        <svg width="400" height="400" viewBox="0 0 400 400">
+        <svg width={size} height={size} viewBox="0 0 400 400">
           <circle cx={80 + 12} cy={145 + 12} r="18" stroke="#111" fill="#333" />
           <PlanetIcon
             planet={Planet.Sun}
