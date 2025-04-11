7ee2a4707f2f009807e184fa4bfc15b039d6ea97
rodurico <ploissken@gmail.com>
2025-04-11 13:19:00 -0300

    add new chart size

--- src/features/user-profile/ChartController.tsx
+++ src/features/user-profile/ChartController.tsx
@@ -3,19 +3,28 @@ import { Grid, useMediaQuery, useTheme } from "@mui/material";
 import ChartView from "@/components/chart/ChartView";
 import ChartDataTable from "@/components/chart/ChartDataTable";
 import ChartCreationMenu from "../../components/chart/ChartCreationMenu";
-import { CHART_SMALL_SIZE, CHART_DEFAULT_SIZE } from "@/lib/chart.consts";
+import {
+  CHART_SMALL_SIZE,
+  CHART_DEFAULT_SIZE,
+  CHART_LARGE_SIZE,
+} from "@/lib/chart.consts";
 
 export default function ChartController() {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
+
+  const chartSize = isSmallScreen
+    ? CHART_SMALL_SIZE
+    : isLargeScreen
+    ? CHART_LARGE_SIZE
+    : CHART_DEFAULT_SIZE;
 
   return (
     <Grid container spacing={2}>
       <ChartCreationMenu />
       <Grid container size={{ xs: 12, lg: 8 }} justifyContent="center">
-        <ChartView
-          size={isSmallScreen ? CHART_SMALL_SIZE : CHART_DEFAULT_SIZE}
-        />
+        <ChartView size={chartSize} />
       </Grid>
       <Grid size={{ xs: 12, lg: 4 }} sx={{ p: 2 }}>
         <ChartDataTable />
--- src/lib/chart.consts.ts
+++ src/lib/chart.consts.ts
@@ -1,4 +1,5 @@
 export const CHART_SMALL_SIZE = 350;
 export const CHART_DEFAULT_SIZE = 400;
+export const CHART_LARGE_SIZE = 500;
 
 export const CHART_PLANET_SIZE = 20;
