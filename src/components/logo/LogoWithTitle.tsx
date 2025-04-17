6d68f7a7e21696b62f17ec69d0c5c34ee41e2da6
rodurico <ploissken@gmail.com>
2025-04-17 15:39:13 -0300

    update logo image'

--- src/components/logo/LogoWithTitle.tsx
+++ src/components/logo/LogoWithTitle.tsx
@@ -3,7 +3,7 @@ import { LogoProps, Logo } from "./Logo";
 
 export function LogoWithTitle({ size = 100, color = "white" }: LogoProps) {
   return (
-    <Grid container sx={{ flexGrow: 1 }}>
+    <Grid container sx={{ flexGrow: 1 }} alignItems="center">
       <Logo size={size} color={color} />
       <Typography variant="h5" component="div">
         mercuryou
